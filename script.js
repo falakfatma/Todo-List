a28ae7783363e747f88235d72a55d694753d096b
Falak Fatma <102820083+falakfatma@users.noreply.github.com>
2023-01-07 16:47:55 +0000

    add button for delete a specific item and make it responsive

--- script.js
+++ script.js
@@ -2,44 +2,63 @@ let btn = document.querySelector(".btn");
 let todosHeading = document.getElementById("taskName")
 let taskListId = document.getElementById("taskListId");
 let todosText = document.getElementById("textarea");
-let div = document.createElement("li");
-div.style.listStyle = 'none'
-//on create 
-btn.addEventListener("click", (event) => {
-  event.preventDefault()
-  localStorage.setItem(todosHeading.value, todosText.value)
- div.innerHTML += 
-   `
-   <h1>${todosHeading.value}</h1>
-   <p> ${todosText.value}</p>
-   `
-  console.log(div)
-  taskListId.append(div)
-  todosHeading.value = '';
-  todosText.value = '';
-})
-//
-// // on delete
-deleteBtn.addEventListener("click", (event) => {
-  event.preventDefault()
-  localStorage.removeItem(todosHeading.value, todosText.value)
- div.innerHTML = ""
-  // console.log(div)
-  taskListId.append(div)
-  todosHeading.value = '';
-  todosText.value = '';
-})
-// // let notes = prompt(`Write your Note`)
-// // // if(todosHeading){
-// confirm(` Its Heading is ${todosHeading}`)
-// // }
-// let clear = confirm(` want to delete your all notes`)
-// let clearone = confirm(` want to delete your note`)
-// if (clearone) {
-//   localStorage.removeItem(todosHeading)
+let deleteBtn = document.getElementById('deleteBtn')
+let deleteItem = document.getElementsByClassName('btn-danger')
 
-// }
-// if (clear) {
-//   localStorage.clear()
-//   alert(`delete successfully`)
-// }
+
+taskListId.style.listStyle = 'none'
+
+// on delete a item 
+function myFunction(e) {
+  let objectKey = localStorage.key(e)
+  localStorage.removeItem(objectKey)
+  setTimeout(() => {
+    location.reload()
+  }, 1000)
+}
+
+// on get
+const getTasks = () => {
+  const tasks = localStorage;
+  for (const [key, value] of Object.entries(tasks)) {
+    taskListId.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between align-items-start">
+      <div class="fw-bold">${key}</div>
+      ${value}
+      <button class="btn btn-danger" onClick=myFunction(todosHeading)>Delete</button>
+    </li>
+   `
+  }
+}
+window.onload = getTasks()
+
+
+
+const createTask = (e) => {
+  e.preventDefault()
+  taskListId.innerHTML +=
+    `
+    <li class="list-group-item d-flex justify-content-between align-items-start">
+         <div class="fw-bold" >${todosHeading.value}</div>
+         ${todosText.value}
+          <button class="btn btn-danger" onclick="myFunction(todosHeading)">Delete</button>
+    </li>
+   `
+  localStorage.setItem(todosHeading.value, todosText.value)
+  todosHeading.value = '';
+  todosText.value = '';
+}
+
+//on create 
+btn.addEventListener("click", createTask)
+
+const deleteAllTask = (e) => {
+  e.preventDefault()
+  localStorage.clear()
+  taskListId.innerHTML = ""
+  todosHeading.value = '';
+  todosText.value = '';
+}
+
+// on delete All
+deleteBtn.addEventListener("click", deleteAllTask)
